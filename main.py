8c9f1725ef4875ed6cf70ab1e9bafe8938527997
Ido Strassberg <idostrassberg123@gmail.com>
2022-10-08 18:08:57 +0300

    Program receive amount of money, lines to bet on and amount to bet with and prints it.

--- main.py
+++ main.py
@@ -4,43 +4,34 @@ MIN_BET = 1
 
 
 def deposit():
-    while True:
-        amount = input("What would you like to deposit? $")
+    amount = input("What would you like to deposit? $")
+    while not amount.isdigit() or int(amount) <= 0:
         if amount.isdigit():
-            amount = int(amount)
-            if amount >0:
-                break
-            else:
-                print("Amount must be greater than 0.")
+            print("Amount must be greater than 0.")
         else:
-            print("Please enter a number.")
-    return amount
+            print("Please enter a valid positive number.")
+        amount = input("What would you like to deposit? $")
+    return int(amount)
 
 def get_number_of_lines():
-    while True:
-        lines = input("Enter the number of lines to ben on (1-" + str(MAX_LINES) + ")? ")
+    lines = input(f"Enter the number of lines to between on (1-{MAX_LINES}) ")
+    while not lines.isdigit() or (int(lines) < 1 or int(lines) > MAX_LINES):
         if lines.isdigit():
-            lines = int(lines)
-        if 1 <= lines <= MAX_LINES:
-            break
-        else:
             print("Enter a valid number of lines")
-    else:
-        print("Please enter a number.")
-    return lines
-
-def get_bet():
-    while True:
-        amount = input("What would you like to bet? $")
-        if amount.isdigit():
-            amount = int(amount)
-            if MIN_BET <= amount <= MAX_BET:
-                break
-            else:
-                print("Amount must be between " + str(MIN_BET) + " and " + str(MAX_BET))
         else:
             print("Please enter a number.")
-    return amount
+        lines = input(f"Enter the number of lines to between on (1-{MAX_LINES}) ")
+    return int(lines)
+
+def get_bet():
+    amount = input("What would you like to bet? $")
+    while not amount.isdigit() or (MIN_BET > int(amount) or MAX_BET < int(amount)):
+        if amount.isdigit():
+            print(f"Amount must be between {MIN_BET} and {MAX_BET}.")
+        else:
+            print("Please enter a number.")
+        amount = input("What would you like to bet? $")
+    return int(amount)
 
 def main():
     balance = deposit()
